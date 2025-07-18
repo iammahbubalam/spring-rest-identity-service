b39cfc159f46204b357b4eff6dac91979f60df52
iammahbubalam <malam212082@bscse.uiu.ac.bd>
2025-07-18 12:02:21 +0600

    Initial commit

---
+++ src/test/java/com/iammahbubalam/rest/identity/SpringRestIdentityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iammahbubalam.rest.identity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestIdentityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
